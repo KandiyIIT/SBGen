0c49852680a527078ab4deb4f0467a29f59e0673
KandiyIIT <sergeykandy@gmail.com>
2022-06-13 22:51:34 +0300

    version 1.0.0

--- SBGenTool/SBGen.cpp
+++ SBGenTool/SBGen.cpp
@@ -41,16 +41,19 @@
 #define delta_uniformity_flag				0x0c
 #define algebraic_immunity_flag		0x0d
 #define seed_flag								0x0e
+#define erase_points_flag					0x0f
 
 #define default_try_per_thread			10000
 #define default_thread_count				1
 #define default_log								0
+#define default_erase_points				0
 
 const struct option longopts[] =
 {
 	{"version",						no_argument,			0,	version_flag},
 	{"visibility",						no_argument,			0,	visibility_flag},
 	{"help",							no_argument,			0,	help_flag},
+	{"erase_points",				no_argument,	0,	erase_points_flag},
 	{"method",						required_argument,	0,	method_flag},
 	{"cost_function",				required_argument,	0,	cost_function_flag},
 	{"thread_count",				required_argument,	0,	thread_count_flag},
@@ -62,6 +65,8 @@ const struct option longopts[] =
 	{"delta_uniformity",			required_argument,	0,	delta_uniformity_flag},
 	{"algebraic_immunity",	required_argument,	0,	algebraic_immunity_flag},
 	{"seed",							required_argument,	0,	seed_flag},
+	{"max_frozen_loops",		required_argument,	0,	max_frozen_loops_flag},
+
 	{0,0,0,0},
 };
 
@@ -187,8 +192,8 @@ get_cost_function(std::map<int, std::string>& options)
 				}
 				
 				try {
-					x = std::stoi(values[0]);
-					r = std::stoi(values[1]);
+					r = std::stoi(values[0]);
+					x = std::stoi(values[1]);
 				}
 				catch (const std::invalid_argument & e) 
 				{
@@ -244,49 +249,50 @@ get_cost_function(std::map<int, std::string>& options)
 		else if(parameter->second == "wcf")
 		{
 			return std::make_pair<std::function< sbgen::cost_info_t<T>(sbgen::cost_function_data_t*, std::array<uint8_t, 256>)>
-				, sbgen::cost_function_data_t*>(sbgen::wcf<T>,nullptr);
+				, sbgen::cost_function_data_t*>(sbgen::wcf<T>,new sbgen::wcf_function_data_t());
 		}
 		ABORT_MSG("Unknown cost function. See help for avaliable cost functions");
 	}
 	return std::make_pair<std::function< sbgen::cost_info_t<T>(sbgen::cost_function_data_t*, std::array<uint8_t, 256>)>
-		, sbgen::cost_function_data_t*>(sbgen::whs<T>,nullptr);
+		, sbgen::cost_function_data_t*>(sbgen::wcf<T>,new sbgen::wcf_function_data_t());
 }
 
-void setup_properties (sbgen::properties_info_t info,  std::map<int, std::string>& options) 
+void setup_properties (sbgen::properties_info_t* info,  std::map<int, std::string>& options) 
 {
+	info->properties_config = 0;
 	try 
 	{
 		auto property = options.find(nonlinearity_flag);
 		if (property != options.end()) 
 		{
-			setup_property( &info, SBGEN_NONLINEARITY, std::stoi(property->second));
+			setup_property( info, SBGEN_NONLINEARITY, std::stoi(property->second));
 		}
 		else
 		{
 			ABORT_MSG("Need target nonlinearity");
 		}
 		
-		options.find(delta_uniformity_flag);
+		property = options.find(delta_uniformity_flag);
 		if (property != options.end()) 
 		{
-			setup_property( &info, SBGEN_DELTA_UNIFORMITY, std::stoi(property->second));
+			setup_property( info, SBGEN_DELTA_UNIFORMITY, std::stoi(property->second));
 		}
 		
-		options.find(algebraic_immunity_flag);
+		property = options.find(algebraic_immunity_flag);
 		if (property != options.end()) 
 		{
-			setup_property( &info, SBGEN_ALGEBRAIC_IMMUNITY, std::stoi(property->second));
+			setup_property( info, SBGEN_ALGEBRAIC_IMMUNITY, std::stoi(property->second));
 		}
 		
-		options.find(seed_flag);
+		property = options.find(seed_flag);
 		if (property != options.end()) 
 		{
-				info.use_random_seed = false;
-				info.seed = std::stoi(property->second);
+				info->use_random_seed = false;
+				info->seed = std::stoi(property->second);
 		}
 		else
 		{
-				info.use_random_seed = true;
+				info->use_random_seed = true;
 		}
 	}
 	catch (const std::invalid_argument & e) 
@@ -297,14 +303,17 @@ void setup_properties (sbgen::properties_info_t info,  std::map<int, std::string
 	{
 		std::cout << e.what() << "\n";
 	}
+	return;
 }
 
 template<typename T>
 void run_generator(std::map<int, std::string>& options)
 {
-	int32_t thread_count = default_try_per_thread;
-	int32_t try_per_thread = default_thread_count;
+	int32_t thread_count = default_thread_count;
+	int32_t try_per_thread = default_try_per_thread;
 	int32_t max_frozen_count = try_per_thread;
+	int32_t visibility = default_log;
+	int32_t erase_points = default_erase_points;
 	
 	auto parameter = options.find(method_flag);
 	if (parameter != options.end()) 
@@ -322,6 +331,17 @@ void run_generator(std::map<int, std::string>& options)
 			property = options.find(max_frozen_loops_flag);
 			if (property != options.end())
 				max_frozen_count = std::stoi(property->second);
+			else
+				max_frozen_count = try_per_thread;
+            
+			property = options.find(visibility_flag);
+			if (property != options.end())
+				visibility = 1;
+			
+			property = options.find(erase_points_flag);
+			if (property != options.end())
+				erase_points = 1;
+
 		}
 		catch (const std::invalid_argument & e) 
 		{
@@ -336,17 +356,56 @@ void run_generator(std::map<int, std::string>& options)
 		{
 			auto cost = get_cost_function<T>(options);
 			sbgen::hill_climbing_info_t<T> info;
+			
 			info.thread_count = thread_count;
 			info.try_per_thread = try_per_thread;
 			info.max_frozen_count = max_frozen_count;
 			info.cost_function = cost.first;
 			info.cost_data.reset(cost.second);
-			setup_properties(static_cast<sbgen::properties_info_t>(info), options);
+			info.is_log_enabled = visibility;
+			setup_properties(static_cast<sbgen::properties_info_t*>(&info), options);
+			
+			std::cout<<"Starting hill climbing..."<<std::endl;
+			std::cout<<"Parameters:"<<std::endl;
+			std::cout<<"Thread count: "<<info.thread_count<<std::endl;
+			std::cout<<"Try per thread: "<<info.try_per_thread<<std::endl;
+			std::cout<<"Max frozen loops: "<<info.max_frozen_count<<std::endl;
+			std::cout<<"Log level: "<<info.is_log_enabled<<std::endl;
+			std::cout<<"Cost Function: "<<info.cost_data->name()<<std::endl;
+			if(info.properties_config & SBGEN_USE_NONLINEARITY_FLAG) {
+				std::cout<<"target NL: "<<info.target_properties[SBGEN_NONLINEARITY]<<std::endl;
+			} else std::cout<<"NL not used\n";
+			if(info.properties_config & SBGEN_USE_DELTA_UNIFORMITY_FLAG) {
+				std::cout<<"target DU: "<<info.target_properties[SBGEN_DELTA_UNIFORMITY]<<std::endl;
+			}else std::cout<<"DU not used\n";
+			if(info.properties_config & SBGEN_USE_ALGEBRAIC_IMMUNITY_FLAG) {
+				std::cout<<"target AI: "<<info.target_properties[SBGEN_ALGEBRAIC_IMMUNITY]<<std::endl;
+			}else std::cout<<"AI not used\n";
+			if(info.use_random_seed==true)
+				std::cout<<"Seed: random"<<std::endl;
+			else
+				std::cout<<"Seed: "<<info.seed<<std::endl;
 			
 			auto sbox = sbgen::hill_climbing<T>(info);
 
-			if (!sbox.has_value()) {
-				PRINT_SBOX(sbox.value());
+			if (sbox.has_value()) 
+			{
+				auto sb = sbox.value();
+				
+				if(erase_points) 
+				{
+						sbgen::transform_utils::erase_fixed_points(sb, info.seed);
+				}
+				
+				PRINT_SBOX(sb);
+				
+				std::cout<<"NL= "<<sbgen::properties::nonlinearity(sb)<<std::endl;
+				std::cout<<"DU= "<<sbgen::properties::delta_uniformity(sb)<<std::endl;
+				std::cout<<"AI= "<<sbgen::properties::algebraic_immunity(sb)<<std::endl;
+				std::cout<<"Fixed Points= "<<sbgen::properties::fixed_points(sb)<<std::endl;
+				
+			} else {
+				ABORT_MSG("SBox not found. Try another parameters or restart.");
 			}
 		} 
 		else if(parameter->second == "simulated_annealing")
@@ -358,7 +417,8 @@ void run_generator(std::map<int, std::string>& options)
 			info.max_frozen_outer_loops = max_frozen_count;
 			info.cost_function = cost.first;
 			info.cost_data.reset(cost.second);
-			setup_properties(static_cast<sbgen::properties_info_t>(info), options);
+			info.is_log_enabled = visibility;
+			setup_properties(static_cast<sbgen::properties_info_t*>(&info), options);
 			
 			parameter = options.find(method_params_flag);
 			if (parameter != options.end()) 
@@ -389,10 +449,50 @@ void run_generator(std::map<int, std::string>& options)
 				ABORT_MSG("Can't find simulated annealing parameters");
 			}
 			
+			std::cout<<"Starting simulated annealing..."<<std::endl;
+			std::cout<<"Parameters:"<<std::endl;
+			std::cout<<"Thread count: "<<info.thread_count<<std::endl;
+			std::cout<<"Max outer loops in thread: "<<info.max_outer_loops<<std::endl;
+			std::cout<<"Max inner loops in thread: "<<info.max_inner_loops<<std::endl;
+			std::cout<<"Initial temperature: "<<info.initial_temperature<<std::endl;
+			std::cout<<"Alpha parameter: "<<info.alpha_parameter<<std::endl;
+			std::cout<<"Max frozen loops: "<<info.max_frozen_outer_loops<<std::endl;
+			std::cout<<"Log level: "<<info.is_log_enabled<<std::endl;
+			std::cout<<"Cost Function: "<<info.cost_data->name()<<std::endl;
+			if(info.properties_config & SBGEN_USE_NONLINEARITY_FLAG) {
+				std::cout<<"target NL: "<<info.target_properties[SBGEN_NONLINEARITY]<<std::endl;
+			} else std::cout<<"NL not used\n";
+			if(info.properties_config & SBGEN_USE_DELTA_UNIFORMITY_FLAG) {
+				std::cout<<"target DU: "<<info.target_properties[SBGEN_DELTA_UNIFORMITY]<<std::endl;
+			}else std::cout<<"DU not used\n";
+			if(info.properties_config & SBGEN_USE_ALGEBRAIC_IMMUNITY_FLAG) {
+				std::cout<<"target AI: "<<info.target_properties[SBGEN_ALGEBRAIC_IMMUNITY]<<std::endl;
+			}else std::cout<<"AI not used\n";
+			if(info.use_random_seed==true)
+				std::cout<<"Seed: random"<<std::endl;
+			else
+				std::cout<<"Seed: "<<info.seed<<std::endl;
+			
 			auto sbox = sbgen::simulated_annealing<T>(info);
 
-			if (!sbox.has_value()) {
-				PRINT_SBOX(sbox.value());
+			if (sbox.has_value()) 
+			{
+				auto sb = sbox.value();
+				
+				if(erase_points) 
+				{
+						sbgen::transform_utils::erase_fixed_points(sb, info.seed);
+				}
+				
+				PRINT_SBOX(sb);
+				
+				std::cout<<"NL= "<<sbgen::properties::nonlinearity(sb)<<std::endl;
+				std::cout<<"DU= "<<sbgen::properties::delta_uniformity(sb)<<std::endl;
+				std::cout<<"AI= "<<sbgen::properties::algebraic_immunity(sb)<<std::endl;
+				std::cout<<"Fixed Points= "<<sbgen::properties::fixed_points(sb)<<std::endl;
+				
+			} else {
+				ABORT_MSG("SBox not found. Try another parameters or restart.");
 			}
 		}
 	}
