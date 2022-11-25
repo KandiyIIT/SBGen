06e4b23b415102c25188e5aa4259822aafe10e62
KandiyIIT <sergeykandy@gmail.com>
2022-11-25 18:35:26 +0200

    add cost functions cf1 and cf2

--- tests/test_hill_climbing.cpp
+++ tests/test_hill_climbing.cpp
@@ -312,7 +312,7 @@ int test_hill_climbing_with_pcf3()
 	sbgen::hill_climbing_info_t<double> info;
 
 	info.thread_count = 1;
-	info.is_log_enabled = false;
+	info.is_log_enabled = true;
 
 	info.try_per_thread = 1000000;
 	info.max_frozen_count = 1000000;
@@ -355,22 +355,138 @@ int test_hill_climbing_with_pcf3()
 	return ACCEPT_TEST;
 }
 
+int test_hill_climbing_with_cf1() 
+{
+
+	sbgen::hill_climbing_info_t<double> info;
+
+	info.thread_count = 8;
+	info.is_log_enabled = false;
+
+	info.try_per_thread = 1000000;
+	info.max_frozen_count = 100000;
+	
+	setup_property( &info, SBGEN_NONLINEARITY, 104);
+
+	
+	info.use_random_seed = true;
+	info.seed = 0xdeadbeef;
+
+
+
+	info.cost_function = sbgen::cf1<double>;
+	info.cost_data.reset(new sbgen::cf1_function_data_t(12, 32, 0));
+
+	auto sbox = sbgen::hill_climbing<double>(info);
+
+	if (!sbox.has_value()) {
+		return REJECT_TEST;
+	}
+
+	if(sbgen::properties::nonlinearity(sbox.value())!=104) {
+		return REJECT_TEST;
+	}
+	
+	//PRINT_SBOX(sbox.value());
+	return ACCEPT_TEST;
+}
+
+int test_hill_climbing_with_cf2_1() 
+{
+
+	sbgen::hill_climbing_info_t<double> info;
+
+	info.thread_count = 1;
+	info.is_log_enabled = false;
+
+	info.try_per_thread = 1000000;
+	info.max_frozen_count = 100000;
+	
+	setup_property( &info, SBGEN_NONLINEARITY, 104);
+
+	
+	info.use_random_seed = false;
+	info.seed = 0xdeadbeef;
+
+	info.cost_function = sbgen::cf2<double>;
+	info.cost_data.reset(new sbgen::cf2_function_data_t(1, 32, 32));
+
+	auto sbox = sbgen::hill_climbing<double>(info);
+
+	if (!sbox.has_value()) {
+		return REJECT_TEST;
+	}
+
+	if(sbgen::properties::nonlinearity(sbox.value())!=104) {
+		return REJECT_TEST;
+	}
+	
+	//PRINT_SBOX(sbox.value());
+	return ACCEPT_TEST;
+}
+
+int test_hill_climbing_with_cf2_2() 
+{
+
+	sbgen::hill_climbing_info_t<uint64_t> info;
+
+	info.thread_count = 1;
+	info.is_log_enabled = false;
+
+	info.try_per_thread = 1000000;
+	info.max_frozen_count = 100000;
+	
+	setup_property( &info, SBGEN_NONLINEARITY, 104);
+
+	
+	info.use_random_seed = false;
+	info.seed = 0xdeadbeef;
+
+	info.cost_function = sbgen::cf2<uint64_t>;
+	info.cost_data.reset(new sbgen::cf2_function_data_t(1, 32, 32));
+
+	auto sbox = sbgen::hill_climbing<uint64_t>(info);
+
+	if (!sbox.has_value()) {
+		return REJECT_TEST;
+	}
+
+	if(sbgen::properties::nonlinearity(sbox.value())!=104) {
+		return REJECT_TEST;
+	}
+	
+	//PRINT_SBOX(sbox.value());
+	return ACCEPT_TEST;
+}
+
 int main()
 {
-	int total_test_count = 6;
+	int total_test_count = 12;
 	int accepted_test_count = 0;
 	
-	run_test(test_hill_climbing_with_whs1, "test hill climbing with whs 1\n", accepted_test_count);
-	run_test(test_hill_climbing_with_whs2, "test hill climbing with whs 2\n", accepted_test_count);
-	//run_test(test_hill_climbing_with_whs3, "test hill climbing with whs 3\n", accepted_test_count);
+	run_test(test_hill_climbing_with_whs1, 
+		"test hill climbing with whs 1\n", accepted_test_count);
+	run_test(test_hill_climbing_with_whs2,
+		"test hill climbing with whs 2\n", accepted_test_count);
+	run_test(test_hill_climbing_with_whs3, "test hill climbing with whs 3\n", accepted_test_count);
 	
-	run_test(test_hill_climbing_with_wcf1, "test hill climbing with wcf 1\n", accepted_test_count);
-	run_test(test_hill_climbing_with_wcf2, "test hill climbing with wcf 2\n", accepted_test_count);
-	//run_test(test_hill_climbing_with_wcf3, "test hill climbing with wcf 3\n", accepted_test_count);
+	run_test(test_hill_climbing_with_wcf1,
+		"test hill climbing with wcf 1\n", accepted_test_count);
+	run_test(test_hill_climbing_with_wcf2,
+		"test hill climbing with wcf 2\n", accepted_test_count);
+	run_test(test_hill_climbing_with_wcf3, "test hill climbing with wcf 3\n", accepted_test_count);
 	
-	run_test(test_hill_climbing_with_pcf1, "test hill climbing with pcf 1\n", accepted_test_count);
-	run_test(test_hill_climbing_with_pcf2, "test hill climbing with pcf 2\n", accepted_test_count);
-	//run_test(test_hill_climbing_with_pcf3, "test hill climbing with pcf 3\n", accepted_test_count);
+	run_test(test_hill_climbing_with_pcf1,
+		"test hill climbing with pcf 1\n", accepted_test_count);
+	run_test(test_hill_climbing_with_pcf2,
+		"test hill climbing with pcf 2\n", accepted_test_count);
+	run_test(test_hill_climbing_with_pcf3, "test hill climbing with pcf 3\n", accepted_test_count);
+	run_test(test_hill_climbing_with_cf1,
+		"test hill climbing with cf1\n", accepted_test_count);
+	run_test(test_hill_climbing_with_cf2_1,
+		"test hill climbing with cf2 1\n", accepted_test_count);
+	run_test(test_hill_climbing_with_cf2_2,
+		"test hill climbing with cf2 2\n", accepted_test_count);
 	
 	printf("%d/%d test passed\n", accepted_test_count, total_test_count);
 	return !(total_test_count==accepted_test_count);
